a8e6a6a013086829b1dd4d42fc71344105d36aae
shijuvar <shiju.varghese@outlook.com>
2014-10-28 15:05:42 +0530

    Added sample for html templates

---
+++ htmltemplates/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+type Task struct {
+	Name  string
+	Description string
+}
+
+
+func main() {
+	http.HandleFunc("/", ShowTasks)
+	http.ListenAndServe(":8080", nil)
+}
+
+func ShowTasks(w http.ResponseWriter, r *http.Request) {
+
+	tasks:=[]Task { Task {"Task1", "Task Desc"}, Task { "Task2","Task Desc"}}
+	fp := path.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := tmpl.Execute(w, tasks); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
