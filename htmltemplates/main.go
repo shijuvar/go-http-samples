932c0fd7c5de287d83d9dc556901a7cf72807736
shijuvar <shiju.varghese@outlook.com>
2014-10-28 17:16:59 +0530

    code refactoring

--- htmltemplates/main.go
+++ htmltemplates/main.go
@@ -10,16 +10,16 @@ type Task struct {
 	Name  string
 	Description string
 }
-
-
 func main() {
 	http.HandleFunc("/", ShowTasks)
 	http.ListenAndServe(":8080", nil)
 }
-
 func ShowTasks(w http.ResponseWriter, r *http.Request) {
 
-	tasks:=[]Task { Task {"Task1", "Task Desc"}, Task { "Task2","Task Desc"}}
+	tasks:=[]Task {
+		{"Task1", "Task Desc"},
+		{ "Task2","Task Desc"},
+	}
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
